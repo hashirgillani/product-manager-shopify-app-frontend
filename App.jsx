b5e3c9759d79e677128f2df2c232de8d2f49dc16
hashirgillani <hashirgillani0@gmail.com>
2026-09-20 02:42:06 +0500

    chore: slean boiler code and add tailwind

--- App.jsx
+++ App.jsx
@@ -1,29 +1,7 @@
-import { BrowserRouter } from "react-router-dom";
-import { useTranslation } from "react-i18next";
-import { NavMenu } from "@shopify/app-bridge-react";
-import Routes from "./Routes";
-
-import { QueryProvider, PolarisProvider } from "./components";
-
 export default function App() {
-  // Any .tsx or .jsx files in /pages will become a route
-  // See documentation for <Routes /> for more info
-  const pages = import.meta.glob("./pages/**/!(*.test.[jt]sx)*.([jt]sx)", {
-    eager: true,
-  });
-  const { t } = useTranslation();
-
   return (
-    <PolarisProvider>
-      <BrowserRouter>
-        <QueryProvider>
-          <NavMenu>
-            <a href="/" rel="home" />
-            <a href="/pagename">{t("NavigationMenu.pageName")}</a>
-          </NavMenu>
-          <Routes pages={pages} />
-        </QueryProvider>
-      </BrowserRouter>
-    </PolarisProvider>
+    <div className="flex min-h-screen items-center justify-center bg-slate-100">
+      <h1 className="text-4xl font-bold text-slate-800">Hello World</h1>
+    </div>
   );
 }
--- index.jsx
+++ index.jsx
@@ -1,9 +1,5 @@
 import App from "./App";
 import { createRoot } from "react-dom/client";
-import { initI18n } from "./utils/i18nUtils";
+import "./index.css";
 
-// Ensure that locales are loaded before rendering the app
-initI18n().then(() => {
-  const root = createRoot(document.getElementById("app"));
-  root.render(<App />);
-});
+createRoot(document.getElementById("app")).render(<App />);
